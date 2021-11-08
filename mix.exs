3d8451d6180d52c65dbae7478d411d4f26881453
gogohhh <gogo_hhh_d@hotmail.com>
2021-11-08 16:18:11 -0600

    desciption

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule People.MixProject do
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env == :prod,
       deps: deps(),
+      description: description(),
       package: package()
     ]
   end
@@ -29,13 +30,18 @@ defmodule People.MixProject do
     ]
   end
 
+  defp description() do
+    """
+    Libreria de configuracion rapida de servidor local de aplicaciones elixir
+    """
+  end
+
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
       name: "fast_server_config",
       # These are the default files included in the package
-      files: ~w(lib src priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib src .formatter.exs mix.exs README* src),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/gogohhh/fast_server_config"}
     ]
